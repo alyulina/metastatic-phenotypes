d4e830e74b1917cd471d7e9122696d57980e0c25
Anastasia Lyulina <30357046+alyulina@users.noreply.github.com>
2023-01-20 20:55:22 -0800

    Update 1_find_barcodes.sh

--- 1_find_barcodes.sh
+++ 1_find_barcodes.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=find_barcodes
 #SBATCH --output=/home/users/alyulina/pdac/bc_counts/novogene_01.09.2023_X202SC22123847-Z01-F001/slurm/slurm_find_barcodes_%A-%a.out
 #SBATCH --partition hns,dpetrov,normal
-#SBATCH --time=1-00:00:00
-#SBATCH --mem=8G
+#SBATCH --time=1:00:00
+#SBATCH --mem=16G
 #SBATCH --array=1-44
 #SBATCH --no-requeue
 #SBATCH --mail-user=alyulina@stanford.edu
