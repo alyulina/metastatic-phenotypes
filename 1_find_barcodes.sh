38e4769f5d5e5066595020d3957f1d6a81973c45
Anastasia Lyulina <30357046+alyulina@users.noreply.github.com>
2023-05-24 15:19:03 -0700

    Updated!!

--- 1_find_barcodes.sh
+++ 1_find_barcodes.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=find_barcodes
-#SBATCH --output=/home/users/alyulina/pdac/bc_counts/novogene_04.26.2023_X202SC23040899-Z01-F001/slurm/slurm_find_barcodes_%A-%a.out
+#SBATCH --output=./slurm/slurm_find_barcodes_%A-%a.out
 #SBATCH --partition hns,dpetrov,normal
 #SBATCH --time=1-00:00:00
 #SBATCH --mem=32G
-#SBATCH --array=1-4
+#SBATCH --array=1-42
 #SBATCH --no-requeue
 #SBATCH --mail-user=alyulina@stanford.edu
 #SBATCH --mail-type=ALL
@@ -19,16 +19,30 @@ module load py-numpy/1.18.1_py36
 
 echo $SLURM_ARRAY_TASK_ID
 
+dir=$PWD
+echo $dir
+
+data="/scratch/users/alyulina/novogene_01.09.2023_X202SC22123847-Z01-F001/01.RawData"
+echo "${data}"
+
 # read sample id
 sample=$(head -$SLURM_ARRAY_TASK_ID samples.txt | tail -1)
 
 # go to where the data is
-cd /scratch/users/alyulina/novogene_04.26.2023_X202SC23040899-Z01-F001/01.RawData/"$sample"/
+cd "${data}"/"$sample"/
 
 # unzip .fq files
 for i in *.fq.gz; do gzip -cd "$i" > "${i%.*}"; done
 
 # rename unzipped .fq files
-for i in *.fq; do mv $i $(echo $i | awk '{split($1,a,/_/); print a[1]"_"a[2]"_R"a[6]}'); done
+for i in *.fq; do cp $i $(echo $i | awk '{split($1,a,/_/); print a[1]"_"a[2]"_R"a[6]}'); done
 
-srun --cpu_bind=verbose python3 /home/users/alyulina/pdac/find_barcodes.py -s $(head -$SLURM_ARRAY_TASK_ID samples.txt | tail -1) -p /scratch/users/alyulina/novogene_04.26.2023_X202SC23040899-Z01-F001/01.RawData/
+# go back
+cd "$dir"
+
+srun --cpu_bind=verbose --wait find_barcodes.py -s "$sample" -p "${data}"/
+wait
+
+cp "${data}"/"$sample"/*_clID_bc_extracted.txt ./out
+cp "${data}"/"$sample"/*_failed_clIDs.txt ./out
+cp "${data}"/"$sample"/*_find_barcodes_stats.txt ./out
