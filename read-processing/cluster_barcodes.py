8508d9053e52d8f923728a04e6a3a0b0f2fca5fd
alyulina <a.s.lyulina@gmail.com>
2025-02-17 16:21:36 -0800

    Small code tweaks

--- read-processing/cluster_barcodes.py
+++ read-processing/cluster_barcodes.py
@@ -17,7 +17,10 @@ sample = options.sample
 sample_path = '/scratch/users/alyulina/novogene_01.09.2023_X202SC22123847-Z01-F001/01.RawData/' + sample + '/'
 bartender_path = '/home/groups/dpetrov/SOFTWARE/bartender-1.1-master/'
 
-clIDs = ['AACC', 'AAGG', 'ACAC', 'ACCT', 'ACGA', 'ACTG', 'AGAG', 'AGCA', 'AGGT', 'AGTC', 'ATCG', 'ATGC', 'CAAC', 'CACT', 'CAGA', 'CATG', 'CCAA', 'CCTT', 'CGAT', 'CGTA', 'CTGT', 'CTTC', 'GAAG', 'GATC', 'GCAT', 'GCTA', 'GGAA', 'GGTT', 'GTAC', 'GTGA', 'GTTG', 'TCCA', 'TGAC', 'TGCT', 'TGTG', 'TTCC', 'TTGG']
+# cell line IDs, same as before, i.e.
+# minumum Hamming distance between any two of them is 2
+# excluded: all but one spike-ins & mt4-2D, which had the same clIDs
+clIDs = ['GATC', 'AAGG', 'ACAC', 'ACCT', 'ACGA', 'ACTG', 'AGAG', 'AGCA', 'AGGT', 'AGTC', 'ATCG', 'ATGC', 'CAAC', 'CACT', 'CAGA', 'CATG', 'CCAA', 'CCTT', 'CGAT', 'CGTA', 'CTGT', 'CTTC', 'GAAG', 'GCAT', 'GCTA', 'GGAA', 'GGTT', 'GTAC', 'GTGA', 'GTTG', 'TCCA', 'TGAC', 'TTCC', 'TTGG']
 
 
 clID_bc_count = [] # a list w/ clID_bc \t count \n
@@ -25,7 +28,7 @@ clID_bc_count = [] # a list w/ clID_bc \t count \n
 # want to have: a merged file for all cell lines for a sample w/ cell line ID, cluster center, and count for this cluster within each cell line
 
 for i in clIDs:
-    barcodes_path = sample_path + sample + '_' + i + '_bc_extracted.txt'
+    barcodes_path = sample_path + sample + '_' + i + '_rBC_extracted.txt'
     out_prefix = sample_path + sample + '_' + i + '_bartender'
     
     if not os.path.exists(barcodes_path):
@@ -56,5 +59,5 @@ for i in clIDs:
 
     clID_bc_count.extend([i + '_' + x[0] + '\t' + str(x[1]) + '\n' for x in cluster_seq_count_i.items()])
 
-with open(sample_path + sample + '_merged_clIDs_bc_clusters_counts.txt', 'w+') as o:
+with open(sample_path + sample + '_clIDs_rBC_cluster_counts.txt', 'w+') as o:
     o.writelines(clID_bc_count)
